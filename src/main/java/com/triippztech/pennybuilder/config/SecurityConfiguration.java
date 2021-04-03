09e8f16e5cf5059aa69e4e80732f99b563b4a06a
Mark Tripoli <mark_tripoli@comcast.com>
2021-04-03 15:04:50 -0400

    modify CSP

--- src/main/java/com/triippztech/pennybuilder/config/SecurityConfiguration.java
+++ src/main/java/com/triippztech/pennybuilder/config/SecurityConfiguration.java
@@ -64,7 +64,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .accessDeniedHandler(problemSupport)
         .and()
             .headers()
-            .contentSecurityPolicy("default-src 'self'; frame-src 'self' data:; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://storage.googleapis.com; style-src 'self' https://fonts.googleapis.com 'unsafe-inline'; img-src 'self' data:; font-src 'self' https://fonts.gstatic.com data:;  script-src 'self' 'unsafe-inline' 'unsafe-eval' https://s3.tradingview.com")
+            .contentSecurityPolicy("default-src 'self'; frame-src 'self' *.tradingview.com data:; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://storage.googleapis.com *.tradingview.com; style-src 'self' https://fonts.googleapis.com *.tradingview.com 'unsafe-inline'; img-src 'self' data:; font-src 'self' https://fonts.gstatic.com data:; script-src-elem 'self' 'unsafe-inline' 'unsafe-eval' *.tradingview.com")
         .and()
             .referrerPolicy(ReferrerPolicyHeaderWriter.ReferrerPolicy.STRICT_ORIGIN_WHEN_CROSS_ORIGIN)
         .and()
